f74956b976ef4151f5e5582aaf3390fed2ae3132
hermandr <hermandr@gmail.com>
2024-12-05 14:49:28 +0800

    Add workspace and VSCode settings, enhance integration tests, and update lookup macros
    - Created a new workspace configuration file for VSCode to streamline development.
    - Added comprehensive settings for editor behavior, file handling, and DBT integration in `.vscode/settings.json`.
    - Introduced new integration test files and updated existing ones to improve data validation and lookup functionality.
    - Refactored lookup macros to enhance flexibility and maintainability, including changes to parameter handling and return values.
    - Updated schema definitions and CSV files to reflect new column names and data types for better consistency across tests.
    
    This commit aims to improve the development environment and enhance the robustness of integration tests.

---
+++ integration_tests/models/dw/test_lookup_min.sql
@@ -0,0 +1,13 @@
+with dim_a as (
+    {{ dbt_utils.stage_sql(ref("dim_a")) }}
+),
+lookup_min as (
+    {{ dbt_utils.lookup_one(
+        main_table='dim_a', col_to_lookup='id_int',
+        lookup_table=ref("tbl_lookup_one_column_id"), lookup_col='id_int',
+        return_col='attr_int', rename_col='attr_int_max',
+        lookup_default='0', return_default='0', 
+        picker_fn='min'
+    )}}
+)
+select * from lookup_min
